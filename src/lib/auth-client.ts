21403ab63b54dd9599163e21daf8e33d73d2a788
Waldo Lavaut <waldothedeveloper@gmail.com>
2025-12-08 12:26:53 -0500

    removed the baseURL since it broke the auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,8 +3,8 @@ import { twoFactorClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   plugins: [twoFactorClient()],
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? `https://${process.env.VERCEL_URL!}`
-      : "http://localhost:3000",
+  // baseURL:
+  //   process.env.NODE_ENV === "production"
+  //     ? `https://${process.env.VERCEL_URL!}`
+  //     : "http://localhost:3000",
 });
